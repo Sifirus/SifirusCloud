521c6df5440220270723e2fbe5db66be014a315c
Шакин Кирилл Дмитриевич <k.shakin@gazpromcps.ru>
2026-03-09 13:56:32 +0300

    profile page and signals

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,27 @@
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django import forms
+
+from users.models import Profile
 
 
 class LoginForm(AuthenticationForm):
     pass
 
+
 class RegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+        labels = {
+            'full_name': 'Отображаемое имя',
+            'bio': 'О себе',
+            'location': 'Город',
+            'avatar': 'Аватар'
+        }
--- users/models.py
+++ users/models.py
@@ -2,4 +2,12 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    avatar = models.ImageField(upload_to="profile_pics/", null=True, blank=True)
 
+    def __str__(self):
+        return f'{self.user.username} - Profile'
