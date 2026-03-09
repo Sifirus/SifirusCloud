ffecbd76cf3211b8c8dc85f49c0e1a43d9ddd90f
Youwid <5w1h19s13m8h@gmail.com>
2026-03-09 17:54:07 +0300

    изменнеие

--- SifirusCloud/wsgi.py
+++ SifirusCloud/wsgi.py
@@ -5,7 +5,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""""""""
+"""
+
+"""
+1111
+"""
 
 import os
 
