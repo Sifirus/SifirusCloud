1c5ebec64225583e760e39ea03531033565163af
Your Name <you@example.com>
2026-03-16 10:21:35 +0300

    Исправил файл с аватаркой

--- users/utils/context_processor.py
+++ users/utils/context_processor.py
@@ -1,7 +1,12 @@
 from users.models import Profile
 
-
 def avatar_context_processor(request):
-    profile = Profile.objects.get(user=request.user)
-    avatar = profile.avatar
+    avatar = None
+    if request.user.is_authenticated:
+        try:
+            profile = Profile.objects.get(user=request.user)
+            avatar = profile.avatar
+        except Profile.DoesNotExist:
+            # Если профиль не создан, можно оставить avatar = None
+            pass
     return {'avatar': avatar}
